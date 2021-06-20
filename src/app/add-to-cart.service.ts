0de16552d836bfd4d7b80e53ed2f4fd0b9c079d9
albertoArmero <alberto@scandit.com>
2021-06-20 12:24:56 +0200

    2 files modified, 1 added

---
+++ src/app/add-to-cart.service.ts
@@ -0,0 +1,8 @@
+export class AddToCartService {
+  getLog(){
+
+    console.log('This is a TEST');
+
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { AddToCartComponent } from '@spartacus/storefront';
 import { AddToCartModule} from '@spartacus/storefront';
 import { CartAddEntry } from '@spartacus/core/src/cart/store/actions/cart-entry.action';
+import { AddToCartService } from './add-to-cart.service';
 
 @Component({
   selector: 'my-app',
@@ -79,6 +80,10 @@ export class AppComponent  {
     //addToCart();
     //let cart = new AddToCart
     //let mycart = new CartAddEntry({cartId:'e4f6b7d5-ea5a-4ea7-a1ce-9f0c1a8e98f3',userId:'anonymous',productCode:'300938',quantity:1});
+    let myservice = new AddToCartService();
+    myservice.getLog();
+
+
 
 
 
