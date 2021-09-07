76d498646027f13de97e7939d41fc861b011fd99
albertoArmero <alberto@scandit.com>
2021-09-07 23:40:23 +0200

    4 files modified, 10 added, 9 deleted

--- src/app/add-to-cart.service.ts
+++ src/app/add-to-cart.service.ts
@@ -1,80 +1,61 @@
 import { HttpClient } from "@angular/common/http";
-import { ActiveCartService, CartActions } from "@spartacus/core";
-import { AddToCartComponent } from "@spartacus/storefront";
+import { ActiveCartService, CartActions, BadRequestHandler, CartVoucherService } from "@spartacus/core";
 import { Barcode } from "scandit-sdk";
-import barcodeScannerComponentHtml from "./barcode-scanner/barcode-scanner.component.html";
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn:'root'})
 export class AddToCartService{
-//private http: HttpClient;
-postId: any;
-errorMessage;
-//export class AddToCartService extends ActiveCartService{ 
 
-constructor(private http: HttpClient, private activeCartService: ActiveCartService){
-
+constructor(private http: HttpClient, private activeCartService: ActiveCartService, private errorVoucher: CartVoucherService){
  }
-
-  getLog(){
-
-    console.log('This is a TEST');
-
-  }
-
-  printBarcode(barcode: Barcode[]){
-
-    console.log(barcode[0].data);
-  }
-
   
+  addProductToCart(barcode: Barcode[]){
+
+    var mapped_product:string = this.mapBarcodeWithProduct(barcode[0].data);
+    this.activeCartService.addEntry(mapped_product,1);
+    //console.log('Product ' + mapped_product + ' has been added to the cart');
+
+}
+
   mapBarcodeWithProduct(barcode_value: string): string{
     var mapped_product:string;
-    if (barcode_value == '5395215092097250130')
-      mapped_product = '300938';
-    if (barcode_value == '5213848038594001059')
-      mapped_product = '553637';
-    if (barcode_value == '5561378049223501222')
-      mapped_product = '1934793';
-    if (barcode_value == '5337894712216009332')
-      mapped_product = '1981415';
-    if (barcode_value == '5181326608309400233')
-      mapped_product = '816780';
 
+    switch(barcode_value) { 
+      case '5395215092097250130':{ 
+        mapped_product = '300938';
+        break; 
+      } 
+      case '5213848038594001059':{ 
+        mapped_product = '553637';
+        break; 
+      }
+      case '5561378049223501222':{ 
+        mapped_product = '1934793';
+        break; 
+      }
+      case '5337894712216009332':{ 
+        mapped_product = '1981415';
+        break; 
+      }
+      case '5181326608309400233':{ 
+        mapped_product = '816780';
+        break; 
+      }   
+      default: { 
+        mapped_product = 'InvalidProductID';
+        this.invalidProduct();
+        break; 
+      } 
+  } 
 
     return mapped_product;
   }
 
-  addProductToCart(barcode: Barcode[]){
+  invalidProduct(){
 
-    var mapped_product:string = this.mapBarcodeWithProduct(barcode[0].data);
-    //this.mapBarcodeWithProduct(barcode[0].data);
-    this.activeCartService.addEntry(mapped_product,1);
-    //this.activeCartService.addEntry('300938',1);
-    console.log('Product ' + mapped_product + ' has been added to the cart');
-    //console.log(barcode[0].data);
-    //console.log(mapped_product);
-
-    // let mycart = new CartActions.CartAddEntry({cartId:'f3d73d65-21c4-4a45-b43f-142e4ad1d65a',userId:'anonymous',productCode:'300938',quantity:1});
+    var cart_ID:string;
+    this.errorVoucher.addVoucher("invalidProductID",cart_ID);
+    
   }
-  
-
-  /*addToCartHTTP(){
-
-        //const headers = { 'Authorization': 'LOBjhYx3jPcC2H1s_eVFVPuWXvw', 'My-Custom-Header': 'foobar' };
-        const headers = { 'Authorization': `Bearer my_Token`};
-
-        //const body = { title: 'Angular POST Request Example' };
-        this.http.post<any>('https://scandit-commerce2011cx.demo.hybris.com/occ/v2/electronics-spa/users/anonymous/carts/f3d73d65-21c4-4a45-b43f-142e4ad1d65a/entries?code=300938&qty=3&lang=en&curr=USD', { headers }).subscribe({            
-                error: error => {
-                this.errorMessage = error.message;
-                console.error('There was an ERROR!', error);
-                }
-        });
-
-
-
-  }*/
-
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from "@angular/common/http";
 
-import { ConfigModule } from '@spartacus/core';
+import { ConfigModule, GlobalMessageConfig, GlobalMessageType, provideConfig } from '@spartacus/core';
 import { translations, translationChunksConfig } from '@spartacus/assets';
 import { B2cStorefrontModule, defaultCmsContentConfig } from '@spartacus/storefront';
 import { AddToCartModule } from '@spartacus/storefront';
@@ -18,6 +18,28 @@ import { AddToCartService } from './add-to-cart.service';
 const licenseKey: string = "AfNA5Wd/RW6pNTmJfRRmQrAlYOjPJs8Ep23NDwdbciySeTCVlGOpvOkrc5Z3R+pQwVhnuN1AuMhuPT7DQn03uyhPVVuXTRopcjTFebp231wiKrWSsSPKCoN6JRXWNLlDukKQrj5mtsUQFoVfgjRrBnAN4FDDAcOGNhYm1UlnbxGySJ5Arr8rDQBO/83d5c4qSFO4Omk8pRKoS+GZbeuNdGspK+/uVoD5UiTTAPn77aYMhoLHUbIxEezoB982fnaSkx0TFE6TMuIQvV9I21EGsSMVENZWVMdQTww/Z/IsyehbngQnYkkHI452ZwsaTcfrI315l51PG+Yx7WZ0wJ5l6V/CVsnZSay8pW2E5CQRXKrl0wdarGJ8MHMx1bsHSW3AZuFug5smvIa6cL8yh220/lkiDebZ6OWhzIxyuXVn02YG3bYv5Ur6DrKeBEF+nFpSYMZiyQHnAbvS9SixmnW6MjqXGhmsOQzraF0G8QcilVrlLCJTZg1uMcwbDMSFHgDyITAluln40KnL4Gm62vBhVBt/nL2eHibhskLftzpsCTj2hK5YSmV67dpdARgQr7riB3G3PcPBbPNTfZFsivqrOP+iMYq6rsjffxBMuTidZEcJULg48lL6ebCY7fREb3wUG0RSwSB7di3BJMyHW5+b4m6pK9vr5pO6Ypa3wqbnZ0fp6hoIsJLPVVBK/FH+aavrk5ObKmj65v9nolB9kWZAB+XCP4Ct/gn7BTmIvm/rj19Z54+Qg4E1frSBXtuCcIwcqcNtyA68A1HsVyuvxrtWrK/UU/QNtUbglulVVAih6eER1emx0SrEZ/YBNtc6th12TgkNWMx+koGFvdBTMQ==";
 const engineLocation: string = "https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build/"; // could also be e.g. "build"
 
+export const translationOverwrites = {
+  en: { 
+    common: { 
+        httpHandlers: { 
+          invalidCodeProvided: 'Product not found in catalogue'
+      },
+     },
+  },
+}
+
+function yourGlobalMessageConfigFactory(): GlobalMessageConfig {
+  return {
+    globalMessages: {
+      [GlobalMessageType.MSG_TYPE_CONFIRMATION]: {
+        timeout: 5000,
+      },
+      [GlobalMessageType.MSG_TYPE_ERROR]: {
+        timeout: 3000,
+      },
+    },
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,7 +53,6 @@ const engineLocation: string = "https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/bui
       occ: {
         baseUrl: 'https://scandit-commerce2011cx.demo.hybris.com/',
         prefix: '/occ/v2/',
-        legacy: false
       }
     },
     authentication: {
@@ -53,9 +74,18 @@ const engineLocation: string = "https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/bui
     ScanditSdkModule.forRoot(licenseKey, {engineLocation}),
     MatIconModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    ConfigModule.withConfigFactory(yourGlobalMessageConfigFactory)
   ],
-  providers: [AddToCartService],
+  providers: [AddToCartService,  provideConfig({
+    i18n: { resources: translationOverwrites },
+  })],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
+
+
+
+
+
